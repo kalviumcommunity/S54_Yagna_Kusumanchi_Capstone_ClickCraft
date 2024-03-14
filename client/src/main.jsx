4a4f294a9304a879b905c52f0e3f354c174f102e
Yagna123k <yagna82kb@gmail.com>
2024-03-14 22:25:42 +0530

    Cleared Auth error in deployement

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+import App from './App';
+
+console.log(import.meta.env);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Auth0Provider
-        domain="dev-jw2h77uc2n0sgdgj.us.auth0.com"
-        clientId="fflUrqt5ZRJFnXhNQA9CgA9sTxgg6nrd"
+        domain={import.meta.env.VITE_AUTH0_DOMAIN || 'dev-jw2h77uc2n0sgdgj.us.auth0.com'}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID || 'fflUrqt5ZRJFnXhNQA9CgA9sTxgg6nrd'}
         authorizationParams={{
-            redirect_uri: window.location.origin
+            redirect_uri: import.meta.env.VITE_CALLBACK_URL || 'http://localhost:5173/'
         }}
     >
+        {console.log(import.meta.env)}
         <App />
-    </Auth0Provider>,
-)
+    </Auth0Provider>
+);
